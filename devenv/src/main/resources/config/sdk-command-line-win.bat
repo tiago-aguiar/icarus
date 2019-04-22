792db04b22000d90538636b2cdb365c97d2eadd2
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2019-04-22 17:43:42 -0300

    windows stuff

---
+++ devenv/src/main/resources/config/sdk-command-line-win.bat
@@ -0,0 +1,23 @@
+@Echo OFF
+
+echo Downloading file...
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip','android-sdk-win.zip')
+powershell.exe -Command Expand-Archive android-sdk-win.zip -DestinationPath %userprofile%/android-sdk
+
+REM TODO será preciso baixar o JRE e empacotar junto do projeto
+SET PATH=%PATH%;c:\Users\tiago\jre1.8.0_211\bin
+
+copy /y nul %userprofile%\.android\repositories.cfg
+
+echo "Start android platform tools"
+call %userprofile%\android-sdk\tools\android update sdk --use-sdk-wrapper --all --no-ui --no-https --filter platform-tools
+echo "Start android 28"
+call %userprofile%\android-sdk\tools\android update sdk --use-sdk-wrapper --all --no-ui --no-https --filter android-28 
+echo "Start sdkmanager sources"
+call %userprofile%\android-sdk\tools\bin\sdkmanager --no_https --verbose "sources;android-28"
+echo "Start sdkmanager images"
+call %userprofile%\android-sdk\tools\bin\sdkmanager --no_https --verbose "system-images;android-28;google_apis_playstore;x86_64"
+echo "Create avd"
+call %userprofile%\android-sdk\tools\bin\avdmanager -v create avd -n icarus_emulator -k "system-images;android-28;google_apis_playstore;x86_64" -g "google_apis_playstore" -d 19 --force
+
+SET PATH=%PATH%;%ANDROID_HOME%/tools;%ANDROID_HOME%/platform-tools
